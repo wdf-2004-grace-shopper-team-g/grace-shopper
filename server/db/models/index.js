da7c1304b09cb8365baed4771d03e233042867ac
Cesar Perez <cpj8826@gmail.com>
2020-06-16 15:51:31 -0400

    added beats model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,10 +1,21 @@
 const User = require('./user')
+const Beats = require('./beats')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
+ * Associations:
+ *
+      User.hasMany(Order)
+      Order.belongsTo(User)
+
+      Order.hasMany(Beats)
+      Beats.hasMany(Order)
+      Order.belongsToMany(Beats, { through: "beat_order" })
+      Beats.belongsToMany(Order, { through: "beat_order" })
+
  */
 
 /**
@@ -14,5 +25,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Beats
 }
