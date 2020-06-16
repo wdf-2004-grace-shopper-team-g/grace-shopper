679a1f2c871eed115c432d1e3d9cb5e9edac05d8
electrone901 <carbajalluis@rocketmail.com>
2020-06-16 15:02:12 -0400

    userModel: added user model

--- script/seed.js
+++ script/seed.js
@@ -8,8 +8,20 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({
+      email: 'cody@email.com',
+      password: '123',
+      firstName: 'Cody',
+      lastName: 'Santos',
+      address: '1200 Broadway NY NY 10038'
+    }),
+    User.create({
+      email: 'murphy@email.com',
+      password: '123',
+      firstName: 'Murphy',
+      lastName: 'Santos',
+      address: '1200 Broadway NY NY 10038'
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
