557679e824febada0fccd0c0d68b65d744cdf99a
Cesar Perez <cpj8826@gmail.com>
2020-06-19 16:27:24 -0400

    Latest Beat Model

--- server/api/beats.js
+++ server/api/beats.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
-const {Beats} = require('../db/models')
+const {Beat} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const beats = await Beats.findAll({
+    const beats = await Beat.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
@@ -18,7 +18,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
   try {
     // const user = await User.findByPk(req.params.id)
-    let beat = await Beats.findOne({where: {id: req.params.id}})
+    let beat = await Beat.findOne({where: {id: req.params.id}})
 
     // console.log("this is USER***", user)
     // user = await user.getAllBeats()
