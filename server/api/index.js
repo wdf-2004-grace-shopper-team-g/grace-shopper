f0462eb612f7adc1ecc440f1233e123700655035
Hector L. Santana-Quintana <luisantanamusic@icloud.com>
2020-06-20 18:00:42 -0400

    adding some dependencies

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('/beat', require('./beats'))
+router.use('/beats', require('./beats'))
 router.use('/orders', require('./orders'))
 
 router.use((req, res, next) => {
